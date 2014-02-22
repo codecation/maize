8e1fe8622fb4192c58b65a98763393252a314e68
Chris Hunt <chrishunt@github.com>
2014-02-22 16:51:28 -0600

    Add #generate-maze, #neighbors, #visited-neighbors

--- src-cljs/maze/core.cljs
+++ src-cljs/maze/core.cljs
@@ -1,6 +1,5 @@
 (ns maze.core)
 
-
 ;; doors - set of sets of pairs of vecs
 ;;  #{
 ;;     #{[1 2] [2 2]}
@@ -15,3 +14,35 @@
 ;; mark as visited
 ;; randomly select adjacent cell to walk to
 ;; add selected 
+
+; {
+;   :doors   => set of sets of locations
+;   :visited => set of locations
+;   :path    => stack of locations
+;   :size    => size of maze
+; }
+
+; 
+
+(defn neighbors [[x y]]
+  (set
+    (for [dx [-1 0 1] dy [-1 0 1]
+          :when (not= (.abs js/Math dx) (.abs js/Math dy))]
+    [(+ x dx) (+ y dy)])))
+
+(defn visitable-neighbors [location visited size]
+  (letfn [(outside-bounds [[x y]]
+            (or
+              (or (< x 0) (> x (dec size)))
+              (or (< y 0) (> y (dec size)))))]
+    (->>
+      (neighbors location)
+      (remove outside-bounds)
+      (remove visited)
+      (set))))
+
+(defn generate-maze [{:keys [path visited doors size]}]
+  {:visited (range (* size size))})
+
+; (defn start []
+;   (generate-maze [[0,0]]))
--- test/maze/core_test.cljs
+++ test/maze/core_test.cljs
@@ -4,3 +4,32 @@
   (:require [cemerick.cljs.test :as t]
             [maze.core :as core]))
 
+(deftest test-neighbors
+  (testing "returns all neighbors for a location"
+    (is (= #{[2 1] [3 2] [2 3] [1 2]}
+           (core/neighbors [2 2])))
+    (is (= #{[0 -1] [1 0] [0 1] [-1 0]}
+           (core/neighbors [0 0])))))
+
+(deftest test-visitable-neighbors
+  (testing "returns all neighbors within bounds of maze"
+    (is (= #{[1 0] [0 1]}
+           (core/visitable-neighbors [0 0] #{} 5)))
+    (is (= #{[4 3] [3 4]}
+           (core/visitable-neighbors [4 4] #{} 5))))
+  (testing "returns all unvisited neighbors"
+    (is (= #{[2 3] [1 2]}
+           (core/visitable-neighbors [2 2] #{[2 1] [3 2]} 5)))))
+
+(deftest test-generate-maze
+  (testing "visits all locations"
+    (is (= 1 (count
+               (:visited
+                 (core/generate-maze {:visited [] :path [[0 0]] :doors #{} :size 1})))))
+    (is (= 4 (count
+               (:visited
+                 (core/generate-maze {:visited [] :path [[0 0]] :doors #{} :size 2}))))))
+  (testing "path is empty"
+    (is (= 0 (count
+               (:path
+                 (core/generate-maze {:visited [] :path [[0 0]] :doors #{} :size 1})))))))
