202886d60e7cb4c47d827d2bfef6fda005b1b132
Chris Hunt <chrishunt@github.com>
2014-02-23 16:14:15 -0600

    Draw the maze on a canvas

--- src-cljs/maze/draw.cljs
+++ src-cljs/maze/draw.cljs
@@ -1,4 +1,40 @@
 (ns maze.draw
   (:require [maze.core :as core]))
 
-(defn draw [walls])
+(def maze-size-in-cells 50)
+(def cell-size-in-pixels 10)
+(def maze-size-in-pixels (* maze-size-in-cells cell-size-in-pixels))
+(def wall-width-in-pixels 2)
+
+(defn- make-context []
+  "Creates the native js context object"
+  (let [canvas (.getElementById js/document "maze-canvas")
+        context (.getContext canvas "2d")]
+    (set! (.-width canvas) maze-size-in-pixels)
+    (set! (.-height canvas) maze-size-in-pixels)
+    (set! (.-fillStyle context) "rgb(0, 0, 0)")
+    context))
+
+(defn line [[[x1 y1] [x2 y2]]]
+  (let [start-point-x (max x1 x2)
+        start-point-y (max y1 y2)
+        [end-point-x end-point-y] (if (= x1 x2)
+                                    [(inc start-point-x) start-point-y]
+                                    [start-point-x (inc start-point-y)])]
+    {:x1 start-point-x :y1 start-point-y :x2 end-point-x :y2 end-point-y}))
+
+(defn draw-line [{:keys [x1 y1 x2 y2]} context]
+  (.beginPath context)
+  (.moveTo context (* x1 cell-size-in-pixels) (* y1 cell-size-in-pixels))
+  (.lineTo context (* x2 cell-size-in-pixels) (* y2 cell-size-in-pixels))
+  (.stroke context))
+
+(defn start []
+  (let [walls (core/generate-maze {:visited #{}
+                                   :path [[0 0]]
+                                   :doors #{}
+                                   :size maze-size-in-cells})
+        lines (map line (map seq walls))
+        context (make-context)]
+    (doseq [line lines]
+      (draw-line line context))))
---
+++ test/maze/draw_test.cljs
@@ -0,0 +1,12 @@
+(ns maze.draw-test
+  (:require-macros [cemerick.cljs.test
+                    :refer (is deftest with-test run-tests testing test-var)])
+  (:require [cemerick.cljs.test :as t]
+            [maze.draw :as draw]))
+
+(deftest line-test
+  (testing "returns vertical line for vertical wall"
+    (is (= {:x1 2 :y1 1 :x2 2 :y2 2} (draw/line (seq #{[1 1] [2 1]})))))
+  (testing "returns horizontal line for a horizontal wall"
+    (is (= {:x1 2 :y1 1 :x2 3 :y2 1} (draw/line (seq #{[2 0] [2 1]}))))))
+
