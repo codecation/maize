0751b0f7c2a815345ae5db8caa627f90e3e5a95d
Chris Hunt <chrishunt@github.com>
2014-02-24 16:43:08 -0600

    Remove unused :as for cemerick.cljs.test

--- test/maze/core_test.cljs
+++ test/maze/core_test.cljs
@@ -1,7 +1,7 @@
 (ns maze.core-test
   (:require-macros [cemerick.cljs.test
                     :refer (is deftest with-test run-tests testing test-var)])
-  (:require [cemerick.cljs.test :as t]
+  (:require cemerick.cljs.test
             [maze.core :as core]))
 
 (deftest test-neighbors
