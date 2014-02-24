19a3ac0978427ff7eaf9434812bb8285308720a5
Chris Hunt <chrishunt@github.com>
2014-02-24 13:42:21 -0600

    Add core.async to our dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "MIT"
             :url "mit"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
   :plugins [[lein-cljsbuild "1.0.2"]
             [com.cemerick/clojurescript.test "0.2.2"]
             [com.cemerick/austin "0.1.4"]]
